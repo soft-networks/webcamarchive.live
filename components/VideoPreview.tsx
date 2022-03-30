5c480e19e4348fa7c72ccf12c8bf7d73f2c4be82
Bhavik Singh <bhaviksingh@gmail.com>
2022-03-29 21:15:52 -0700

    trying to fix the server video issue

--- components/VideoPreview.tsx
+++ components/VideoPreview.tsx
@@ -4,18 +4,18 @@ import { useMergedVideo, VIDEO_LENGTH } from "../providers/MergedVideoProvider";
 import { useAllVideos } from "../providers/AllVideoProvider";
 import { TEST_STATIC_URL } from "../lib/testData";
 
-
 const VS = [1080, 720];
 interface VideoPreviewProps {
   videoNumber: number;
 }
 const VideoPreview: React.FC<VideoPreviewProps> = ({ videoNumber }) => {
   const { videoList } = useMergedVideo();
-  const {getVideoById} = useAllVideos();
+  const { getVideoById } = useAllVideos();
   return (
     <div>
       {videoList.map((video, index) => (
-          <VideoPreviewPlayer key={index} video={ video ? getVideoById(video) : null} playing={index === videoNumber} /> ))}
+        <VideoPreviewPlayer key={index} video={video ? getVideoById(video) : null} playing={index === videoNumber} />
+      ))}
     </div>
   );
 };
@@ -28,24 +28,32 @@ interface VideoPreviewPlayerProps {
 const VideoPreviewPlayer: React.FunctionComponent<VideoPreviewPlayerProps> = ({ playing, video }) => {
   const videoPlayerRef = useRef<HTMLVideoElement | null>(null);
   const [muteVids, setMuteVids] = useState<boolean>(true);
+  const [playerPlayedOnce, setPlayerPlayedOnce] = useState<boolean>(false);
+
   useEffect(() => {
     document.addEventListener("click", () => setMuteVids(false));
-  })
+  });
   useEffect(() => {
     if (videoPlayerRef.current) {
       if (playing) {
         try {
-          videoPlayerRef.current.play();
+          videoPlayerRef.current.play().then(() => setPlayerPlayedOnce(true));
         } catch (e) {
           console.error("you need to click somewhere first");
         }
       } else {
-        videoPlayerRef.current.pause();
+        if (playerPlayedOnce) videoPlayerRef.current.pause();
       }
     }
-  }, [playing]);
+  }, [playing, playerPlayedOnce]);
   return (
-    <video muted={muteVids || (video === null)} src={video ? video.videoSrc : TEST_STATIC_URL} ref={videoPlayerRef} className={classnames({ hide: !playing, previewVideo: true })} />
+    <video
+      muted={muteVids || video === null}
+      src={video ? video.videoSrc : TEST_STATIC_URL}
+      ref={videoPlayerRef}
+      className={classnames({ hide: !playing, previewVideo: true })}
+      preload={"auto"}
+    />
   );
 };
 
