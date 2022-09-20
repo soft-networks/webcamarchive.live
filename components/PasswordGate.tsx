a96413605db0b68923dc257dd658f33d92eb5d24
Bhavik Singh <bhaviksingh@gmail.com>
2022-09-20 11:04:35 -0400

    A few tweaks to make it more useful post show

--- components/PasswordGate.tsx
+++ components/PasswordGate.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
 import { ReactNode, useRef, useState } from "react";
 import Draggable from "react-draggable";
 
@@ -42,25 +43,59 @@ const PasswordDialog: React.FC<PasswordDialogProps> = ({ onSubmit }) => {
     if (!onSubmit(password)) {
       setIsWrong(true);
       setTimeout(() => setIsWrong(false), 1000);
-    } 
-    
+    }
   };
   return (
-    <Draggable nodeRef={myRef}>
-      <div className="password-overlay app" id="password" ref={myRef}>
-        <div className="handle">
-          <div className="icon"> 
-            <img src="/icons/drag.svg" alt="drag window"/>
+    <>
+      <AltDialog />
+      <Draggable nodeRef={myRef}>
+        <div className="password-overlay app" id="password" ref={myRef}>
+          <div className="handle">
+            <div className="icon">
+              <img src="/icons/drag.svg" alt="drag window" />
+            </div>
+            <div className="title"> Ticket please </div>
           </div>
-          <div className="title"> Ticket please </div>
+          <div className="stack padded">
+            <input
+              tabIndex={10}
+              className="padded"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="password please?"
+            />
+            <div
+              onClick={submitPassword}
+              onKeyDown={(e) => e.key === "Enter" && submitPassword()}
+              className="button align-end"
+              tabIndex={11}
+            >
+              submit
+            </div>
+            {isWrong && <div className="wrong"> wrong password </div>}
+          </div>
         </div>
-        <div className="stack padded">
-          <input tabIndex={10} className="padded" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password please?" />
-          <div onClick={submitPassword} onKeyDown={e => e.key === 'Enter' && submitPassword()} className="button align-end" tabIndex={11}>
-            submit
+      </Draggable>
+    </>
+  );
+};
+
+const AltDialog: React.FC = () => {
+  const myRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <Draggable nodeRef={myRef}>
+      <div className="app" id="info" ref={myRef} style={{ top: "40%" }}>
+        <div className="handle">
+          <div className="icon">
+            <img src="/icons/drag.svg" alt="drag window" />
           </div>
-          {isWrong && <div className="wrong"> wrong password </div>}
-          </div>
+          <div className="title"> Info </div>
+        </div>
+        <div className="padded lightFill" style={{ width: "42ch" }}>
+          Welcome to webcamarchive.live - a collaborative video editor for the work of Molly Soda. If you dont have a
+          ticket, you can visit our <Link href="/preview">preview page</Link> to see what our editors have been up to.
+        </div>
       </div>
     </Draggable>
   );
